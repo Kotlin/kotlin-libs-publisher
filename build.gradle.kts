a9e0ab9ec36d53390edd1ee677917602db4ae153
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-02-18 15:24:17 +0300

    Add Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "ru.ileasile"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
