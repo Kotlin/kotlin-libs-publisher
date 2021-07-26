e6d338ebc2f74d6f3a862dc592f1e388261495f1
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-07-26 15:00:45 +0300

    Update dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
 
