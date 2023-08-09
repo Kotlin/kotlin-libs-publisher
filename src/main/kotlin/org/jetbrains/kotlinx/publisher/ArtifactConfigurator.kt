17ffbb488f30445eca705c09eb8b7b7a63f3fb30
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2023-08-09 13:51:36 +0200

    Remove nullability from ArtifactConfigurator interface

--- src/main/kotlin/org/jetbrains/kotlinx/publisher/ArtifactConfigurator.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/publisher/ArtifactConfigurator.kt
@@ -5,15 +5,15 @@ import org.gradle.api.publish.maven.MavenArtifact
 import org.gradle.api.publish.maven.MavenPublication
 
 class ArtifactConfigurator(private val mavenPublication: MavenPublication) {
-    fun from(component: SoftwareComponent?) {
+    fun from(component: SoftwareComponent) {
         mavenPublication.from(component)
     }
 
-    fun artifact(source: Any?): MavenArtifact? {
+    fun artifact(source: Any): MavenArtifact {
         return mavenPublication.artifact(source)
     }
 
-    fun artifact(source: Any?, config: MavenArtifact.() -> Unit): MavenArtifact? {
+    fun artifact(source: Any, config: MavenArtifact.() -> Unit): MavenArtifact {
         return mavenPublication.artifact(source, config)
     }
 }
